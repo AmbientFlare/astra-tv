d3a5e0b82e88992a1a1bebe089d80b26cf145523
AmbientFlare <admin@ambientflare.com>
2026-06-28 23:13:49 -0700

    Fix Jellyfin playback startup

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -100,7 +100,6 @@ export const RootNavigator = () => {
         break;
       case 'player':
         resetExitPresses();
-        setRoute('detail');
         break;
       case 'home':
       case 'loading':
