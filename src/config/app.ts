792ce1a5c8ae98e873a7a80c17c9e6dd7e802f05
AmbientFlare <admin@ambientflare.com>
2026-07-13 13:49:58 -0700

    Bump version to 1.0.5

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.0.1';
-export const BUILD_NUMBER = '20260707.1';
-export const BUILD_DATE = '2026-07-07';
+export const APP_VERSION = '1.0.5';
+export const BUILD_NUMBER = '20260713.1';
+export const BUILD_DATE = '2026-07-13';
